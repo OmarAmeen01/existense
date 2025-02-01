d6cf658c0aff8f56d830fb7ef65574c9846e3ece
OmarAmeen01 <forcodingcoding@gmail.com>
2025-02-01 17:45:40 +0530

    pushing the code

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
 <section className='mt-28 mx-4'>
         <Image className='object-cover w-full h-52 min-[676px]:h-[550px] rounded-lg shadow-2xl' src="/img6.jpg" alt='product' width={1100} height={669}/>
 </section>
-<section className='mt-28 flex flex-col min-[676px]:flex-row min-[676px]:justify-around  lg:px-28 p-2 '>
+<section className='mt-28 flex flex-col min-[676px]:flex-row min-[676px]:justify-around  lg:px-28  '>
        
        <div className='  min-[676px]:flex min-[676px]:flex-col min-[676px]:justify-center min-[676px]:items-center p-4' >
         <h3 className='font-sans text-xl min-[676px]:text-3xl  font-[600]'>Our vision for the future of education.</h3>
