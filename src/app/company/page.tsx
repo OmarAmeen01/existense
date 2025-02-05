8318f0fe37fba0a90edaa64475badeceb41658b0
OmarAmeen01 <forcodingcoding@gmail.com>
2025-02-05 19:28:42 +0530

    4th night

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -3,107 +3,118 @@ import Image from 'next/image'
 import Footer from '@/components/Footer'
 import CallToAction from '@/components/ui/CallToAction'
 export default function page() {
+
+// styles 
+
+const detailSec = {
+      heading:`font-sans 
+  text-2xl sm:text-3xl min-md:mb-7   font-[500]`,
+  para:`font-sans text-[17px]  py-4 sm:py-6 `,
+  subSection:{
+        heading:`font-sans 
+ text-xl sm:text-2xl min-md:mb-7 font-[500]`,
+    para:`font-sans text-lg py-3 `
+  },
+  conclusion:{
+   heading:`font-sans 
+  text-3xl  min-md:mb-7   font-[500]`,
+   subHeading:`font-sans 
+   text-xl font-[500] sm:text-2xl min-md:mb-7 `,
+  }
+}
+
   return (
      <main>
-    <article className='px-2 min-[390px]:px-4 min-[1200px]:px-36 '>
-    <header className='mt-16 md:w-[44rem] m-auto'>
+    <article className='px-4 min-[1200px]:px-36  '>
+    <header className='mt-4 md:w-[44rem] m-auto'>
 
-<h1  className='font-sans font-[500] text-5xl min-[676px]:text-[56px] text-center  py-4'>About Us</h1>
-<p className='text-center  font-sans  py-4 max-[400px]:px-4 text-lg md:w-[32rem] m-auto' >An initiative with very advanced and initiative technology, aiming in providing meaningful AI solutions</p>
+<h1  className='font-sans font-[500] text-3xl sm:text-4xl md:text-5xl  text-center  py-4'>About Us</h1>
+<p className='text-center  min-[480px]:w-[24rem] font-sans  py-4 max-[400px]:px-4 text-lg sm:w-[32rem] m-auto' >An initiative with very advanced and initiative technology, aiming in providing meaningful AI solutions</p>
 </header>
 
-<section className='mt-20  min-[1350px]:px-40 px-4 '>
-        <Image className='object-cover w-full h-80  md:h-[508px]  rounded-lg shadow-2xl' src="/img6.jpg" alt='product' width={100000} height={669}/>
+<section className='mt-12  min-[1350px]:px-40  '>
+        <Image className='object-cover w-full h-80  md:h-[645px]  max-[370px]:h-36 max-[460px]:h-52 rounded-lg ' src="/img8.webp" alt='product' width={100000} height={669}/>
 </section>
-<section className='justify-between flex flex-col  md:flex-row rounded-lg bg-white px-3 min[400px]:px-10 my-28 mx-3'>
+<section className='justify-between flex flex-col-reverse  min-[700px]:flex-row rounded-lg bg-white min[400px]:px-10 my-20 min-[700px]:mx-10'>
        
-       <div className='  min-[676px]:flex min-[676px]:flex-col min-[676px]:justify-center min-[676px]:items-center   md:w-[20rem] lg:w-[34rem] 2xl:ml-52 md:mx-16 lg:mx-32 lg:max-w-[20rem] ' >
+       <div className='  min-[700px]:flex sm:flex-col  min-[700px]:justify-center min-[700px]:items-center min-[700px]:w-[18rem] lg:w-[34rem] 2xl:ml-52 min-[700px]:mx-8 lg:mx-32 lg:max-w-[20rem] px-4 ' >
         <h3 className='font-sans 
-    max-[450px]:text-[26px] text-3xl md:text-2xl lg:text-3xl min-md:mb-7 max-md:pt-12 font-[500]'>Our vision for the future of education.</h3>
-        <p className='font-sans  py-4 max-[400px]:px-4 text-lg    
+ text-2xl lg:text-3xl min-md:mb-7  max-md:pt-8 font-[500]'>Our vision for the future of education.</h3>
+        <p className='font-sans  py-8 max-md:py-4 text-lg    
          
         '>To revolutionize personalized learning through AI by creating an emotionally intelligent AI tutor that can transform how we learn.</p>
       </div>
     <div>
 
       
-    <Image className=" w-full    max-md:mt-14 rounded-lg lg:w-[40rem] shadow-lg md:h-[448px] min-[1450px]:h-[600px]  max-w-full"    src='/img7.jpg' alt="vision" width={342} height={400} quality={100} style={{objectFit:"cover"}}/>
+    <Image className=" w-full    rounded-lg lg:w-[40rem] min-[700px]:h-[380px]  lg:h-[448px] min-[1450px]:h-[600px]  max-w-full"    src='/img7.jpg' alt="vision" width={342} height={400} quality={100} style={{objectFit:"cover"}}/>
     </div>
     
 </section>
 
-<section className="mt-[72px] min-[676px]:mt-28 px-4 sm:px-10 md:px-40 min-[1200px]:px-72">
+<section className="mt-[72px] min-[676px]:mt-28 min-[500px]:px-16 px-4 sm:px-28 md:px-36 lg:px-56 2xl:px-96">
     <div >
-        <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>The Company</h2>
-        <p  className='font-sans text-lg py-4  '>
+        <h2 className={detailSec.heading}>The Company</h2>
+        <p  className={detailSec.para}>
             We believe that empowering young minds with knowledge and skills is the moral priority of our time. The most meaningful impact can come from dedicating our resources to developing technologies that revolutionize education. In the coming age, advancements in Artificial Intelligence (AI) and personalized learning will reshape how we teach and learn. By contributing in these early stages, we can set the course for a brighter, more equitable future for students everywhere.</p>
-            <p className='font-sans text-lg py-4  '>Hence our goal is to create a truly personalized AI-powered learning platform that transforms education, enabling students to achieve their full potential and fostering a generation equipped to tackle the challenges of tomorrow. Misty’s personalized approach will redefine learning experiences, making them accessible, engaging, and impactful. </p>
-            <p className='font-sans text-lg py-4  '>
+            <p className={detailSec.para}>Hence our goal is to create a truly personalized AI-powered learning platform that transforms education, enabling students to achieve their full potential and fostering a generation equipped to tackle the challenges of tomorrow. Misty’s personalized approach will redefine learning experiences, making them accessible, engaging, and impactful. </p>
+            <p className={detailSec.para}>
             Our company journey will take years of relentless effort — requiring a passionate team dedicated to the mission, significant investment, and groundbreaking innovation to achieve a transformative impact on global education. We face immense challenges and a high likelihood of setbacks. However, if we succeed, we have the potential to redefine the future of learning and build one of the most impactful educational platforms in the world.
             </p>
     </div>
     <div>
-    <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>The Present</h2>
-    <p className='font-sans text-lg py-4  '>
+    <h2 className={detailSec.heading}>The Present</h2>
+    <p className={detailSec.para}>
     Today, we are witnessing unprecedented challenges in education. Millions of students globally lack access to quality learning resources or personalized guidance. This gap is further widened by a shortage of skilled educators, making it increasingly difficult for schools and institutions to scale their impact. If we want to ensure a brighter future for every student, we need more inclusivity and personalized engagement — and this means leveraging intelligent automation and technology.
             </p>
     </div>
     <div>
-    <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>The Possibility</h2><p className='font-sans text-lg py-4  '>
+    <h2 className={detailSec.heading}>The Possibility</h2><p className={detailSec.para}>
     We are in the early stages of a revolution in AI, and this presents an extraordinary opportunity to redefine how we learn and teach. In education, this transformation promises to enhance both the accessibility and quality of learning, creating a future where personalized and scalable education becomes the norm.
             </p>
-            <p className='font-sans text-lg py-4  '>
+            <p className={detailSec.para}>
             As AI integrates into the education sector, the traditional one-size-fits-all model will evolve. Intelligent educational platforms like us, designed to think, learn, and respond to student&aposs unique needs, can deliver tailored learning experiences better than conventional methods. Today, the cost of education is heavily influenced by manual teaching efforts, including teacher salaries and administrative overhead, which account for a significant share of global education spending. However, as AI-powered platforms “join the teaching workforce,” they will reduce these costs by offering scalable, high-quality instruction that is accessible to all.
             </p>
-            <p className='font-sans text-lg py-4  '>
+            <p className={detailSec.para}>
             Over time, these intelligent systems could handle repetitive tasks like grading, curriculum design, and progress tracking, allowing human educators to focus on mentorship and fostering creativity. Eventually, AI-powered platforms could even take on the full spectrum of teaching responsibilities, ensuring every child receives a personalized education that adapts to their learning style and pace.
             </p>
-            <p className='font-sans text-lg py-4  '>
+            <p className={detailSec.para}>
             This shift could bring an abundance of affordable, high-quality educational resources to every corner of the world. With Misty at the forefront, the possibilities are vast: a future where education is no longer limited by geography, affordability, or resource availability. This revolution can democratize learning and empower the next generation with knowledge, skills, and opportunities that were previously out of reach.
             </p>
     </div>
     <div>
-    <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>How we can do it</h2>
-    <p className='font-sans text-lg py-4  '>
+    <h2 className={detailSec.heading}>How we can do it</h2>
+    <p className={detailSec.para}>
     In over a decade of technological advancements, few industries hold as much transformative potential as education enhanced by AI. The future of personalized, scalable learning depends on overcoming significant challenges in technology and infrastructure. At Misty, we are heads-down and focused on making substantial advancements to achieve this vision. Here’s how we plan to get there:
             </p>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>1. Adaptive System Design:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.subSection.heading}>1. Adaptive System Design:</h3>
+            <p className={detailSec.subSection.para}>
             We are building a fully integrated AI education platform, capable of understanding, teaching, and responding to student&aposs unique needs. The goal is to develop a system with the intellectual and emotional capabilities of an exceptional human tutor. We measure this through factors like learning adaptability, personalized content delivery, and a student’s rate of progress. Continuous cycles of development and testing allow us to refine these features for maximum impact.
             </p>
          </div>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>2. Accesibility and Affordability:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.subSection.heading}>2. Accesibility and Affordability:</h3>
+            <p className={detailSec.subSection.para}>
            To make this accessible to all learners, we are working to reduce operational costs through scalable cloud infrastucture and efficient AI model training. By optimizing resources and leveraging economies of scale, we aim to bring Misty&aposs platform to students gobally at an affordabe price point, ensuring equitable access to high-quality education.
             </p>
          </div>
          
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>3. Safety and Trustworthiness:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.subSection.heading}>3. Safety and Trustworthiness:</h3>
+            <p className={detailSec.subSection.para}>
             In education, trust and safety are paramount. We will adhere to strict data privacy standards and ethical AI guidelines, ensuring that student&aposs information is protected and used responsibly. Our platform will include rigorous content moderation and bias mitigation to create a safe and inclusive learning environment.
             </p>
          </div>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>4. Scalable Deployment:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.subSection.heading}>4. Scalable Deployment:</h3>
+            <p className={detailSec.subSection.para}>
             We are preparing for large-scale adoption by designing our platform to handle millions of simultaneous users without compromising quality. From robust server infrastructure to user-friendly interfaces, every element is being developed with scalability in mind. This includes seamless integration with devices students already use, such as laptops, tablets, and smartphones.
             </p>
          </div>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>5. Advanced AI Capabilities:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.subSection.heading}>5. Advanced AI Capabilities:</h3>
+            <p className={detailSec.subSection.para}>
             Building an AI system that can autonomously teach, assess, and adapt is one of the most complex challenges we face. To tackle this, we incorporate state-of-the-art natural language processing, emotion recognition, and adaptive learning algorithms. These enable Misty to provide a truly personalized education, capable of engaging with students in real-time and helping them overcome learning obstacles.
             </p>
          </div>
@@ -111,43 +122,39 @@ export default function page() {
             
     </div>
 </section>
-<section className="mt-28 p-2 min-[500px]:p-10 rounded-lg mx-3 min-[500px]:mx-10 md:px-32 min-[1200px]:mx-20 min-[1200px]:px-52 bg-white ">
+<section className="mt-16 p-2 min-[400px]:px-4 min-[500px]:p-10 rounded-lg mx-3 min-[500px]:mx-10 md:px-32 min-[1200px]:mx-20 min-[1200px]:px-52 bg-white ">
     <header className='mt-10'>
-    <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>Conclusion</h2>
+    <h2 className={detailSec.conclusion.heading}>Conclusion</h2>
     <p className='font-sans text-lg py-4  '>
     In summary, here is the first phase of Our Master Plan
             </p>
     </header>
 
-    <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>1. Build a Compreheve, AI-Powered Education Platform:</h3>
-            <p className='font-sans text-lg py-4  '>
+    <div className='mt-2'>
+         <h3 className={detailSec.conclusion.subHeading}>1. Build a Compreheve, AI-Powered Education Platform:</h3>
+            <p className={detailSec.subSection.para}>
             Develop a feature-complete solution capable of delivering personalized, high-quality learning experiences to every K-12 student.
             </p>
          </div>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>2. Achieve Human-Like Interaction and Engagement:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.conclusion.subHeading}>2. Achieve Human-Like Interaction and Engagement:</h3>
+            <p className={detailSec.subSection.para}>
             Create an emotionally intelligent AI that understands, adapts to, and engages with students just like a dedicated human tutor, fostering a more effective and enjoyable learning experience.
             </p>
          </div>
-         <div className='mt-8'>
-         <h3 className='font-sans 
-    max-[400px]:text-[26px] text-2xl min-md:mb-7 font-[500]'>3. Integrate this into Everyday Learning:</h3>
-            <p className='font-sans text-lg py-4  '>
+         <div className='mt-2'>
+         <h3 className={detailSec.conclusion.subHeading}>3. Integrate this into Everyday Learning:</h3>
+            <p className={detailSec.subSection.para}>
             Seamlessly incorporate this into the lives of students, making it an essential tool for education both at home and in schools, transforming the way knowledge is accessed and applied.
             </p>
-            <p className='font-sans text-lg py-4  '>
+            <p className={detailSec.subSection.para}>
             We have the potential to redefine education and improve the lives of millions of students globally.
             </p>
            
          </div>
-         <div className='mt-8'>
+         <div className='mt-2'>
          <h2 className='font-sans 
-    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 font-[500]'>It’s time to build.</h2>
+    max-[400px]:text-[26px] text-3xl min-md:mb-7  pt-12 max-sm:py-6 font-[500]'>It’s time to build.</h2>
          </div>
 </section>
    <CallToAction/>
