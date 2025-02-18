12465f2f0b6a9f6d2516208dd0e5ecac815f1041
OmarAmeen01 <forcodingcoding@gmail.com>
2025-02-18 15:55:36 +0530

    18th feb 2nd attemp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,18 +20,18 @@ export default function Footer() {
 
   ]
   return (
-    <footer className='flex min-[676px]:flex-row-reverse flex-col justify-center items-center gap-5 min-[676px]:justify-between min-[676px]:px-40 py-10'>
-      <h2 className='font-sans  max-[676px]:font-[600]  min-[676px]:hidden py-2  '>English (US)</h2>
-     <section className='flex min-[676px]:gap-8  '>
+    <footer className='flex min-[800px]:flex-row-reverse flex-col justify-center items-center gap-5 min-[800px]:justify-between min-[800px]:px-40 py-10'>
+      <h2 className='font-sans  max-[800px]:font-[600]  min-[800px]:hidden py-2  '>English (US)</h2>
+     <section className='flex min-[800px]:gap-8  '>
       {socialIcons.map(icon=>{
         return (
-          <a href="#" target='_blank'  key={icon.title}>  <Image src={icon.icon} alt={icon.title}className='hover:opacity-70   max-[676px]:mx-5' width={24} height={24}/></a>
+          <a href="#" target='_blank'  key={icon.title}>  <Image src={icon.icon} alt={icon.title}className='hover:opacity-70   max-[800px]:mx-5' width={24} height={24}/></a>
         )
       })}
 
      </section>
      <section>
-      <h2 className='font-sans font-[600] text-center max-[676px]:hidden py-2'>English (US)</h2>
+      <h2 className='font-sans font-[600] text-center max-[800px]:hidden py-2'>English (US)</h2>
       <p className='font-sans text-sm t  flex gap-1'>Existense <Image src="/cc.svg" className='hover:opacity-70' alt="Existense copyright" width={12} height={12}/> 2025-2026</p>
      </section>
 
